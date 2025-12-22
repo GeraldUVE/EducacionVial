4c8b5211c60b9389985c423436bc167f1b524792
Gerald <Gerald@proyect>
2025-12-21 21:25:00 -0500

    Cambio v2

--- EducacionVial/IniSe.cs
+++ EducacionVial/IniSe.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace EducacionVial
 {
-    public partial class Form1 : Form
+    public partial class IniSe : Form
     {
-        public Form1()
+        public IniSe()
         {
             InitializeComponent();
         }
